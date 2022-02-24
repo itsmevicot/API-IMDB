ff98de03511b6e6ceb83b4fbc42db70844db15e1
Victor Matheus <victor.matheus@protechsolutions.local>
2022-02-24 12:08:39 -0300

    adicionado notificationHandler e update no MovieService

---
+++ imdbEstagioDefinitivo/Service/Interfaces/INotificationHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    public interface INotificationHandler
+    {
+        void NotificarErro(string erro);
+        bool TemErros();
+        List<string> ObterErros();
+    }
+}
--- imdbEstagioDefinitivo/Service/Services/MovieService.cs
+++ imdbEstagioDefinitivo/Service/Services/MovieService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Domain.Interfaces.Repositories;
+using Service.Dtos.MovieDTO;
 using Service.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -13,7 +14,29 @@ namespace Service.Services
     {
         private readonly IMapper _mapper;
         private readonly IMovieRepository _movieRepository;
-    }
+        private readonly INotificationHandler _notificationHandler;
+
+        public MovieService(IMapper mapper, IMovieRepository movieRepository, INotificationHandler notificationHandler)
+        {
+            _mapper = mapper;
+            _movieRepository = movieRepository;
+            _notificationHandler = notificationHandler;
+        }
+
+        public async Task<ReadMovieDto> GetById(int id)
+        {
+            var movie = await _movieRepository.GetById(id);
+            if (movie == null)
+            {
+                _notificationHandler.NotificarErro("O filme buscado não existe.");
+            }
+
+            var mappedMovie = _mapper.Map<ReadMovieDto>(movie);
+
+            return mappedMovie;               
+        }
+      
+    }   
 
   
 
